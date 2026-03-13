4afb8d46176988e54e5796ae0d55053c6d70809c
Se Joon Park <sparky9043@gmail.com>
2026-03-13 15:17:58 +0900

    feat: create function that returns nth fibonacci number

--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,29 @@
-def calculate_fibonacci(n):
+"""
+Calculates the nth number in the Fibonacci sequence.
+Example:
+calculate_fibonacci(0) -> 0
+calculate_fibonacci(1) -> 1
+calculate_fibonacci(5) -> 5
+"""
+# Student must write their code here.
+# For now, let's provide a placeholder that will fail the test.
+
+def calculate_fibonacci(n: int) -> int:
+    """Function takes an integer and returns the fibonacci number in
+    the order specified by the said integer
+
+    Args:
+        n (int): integer that asks for nth Fibonacci number
+
+    Returns:
+        int: Returns the nth Fibonacci number
     """
-    Calculates the nth number in the Fibonacci sequence.
-    Example:
-    calculate_fibonacci(0) -> 0
-    calculate_fibonacci(1) -> 1
-    calculate_fibonacci(5) -> 5
-    """
-    # TODO: Student must write their code here.
-    # For now, let's provide a placeholder that will fail the test.
-    return -1
+    first = 0
+    second = 1
+    i = 0
+    while i < n:
+        copy = first
+        first = second
+        second = copy + second
+        i += 1
+    return first
